7b19886d6c0597787de67ec276ac0ee51100aa74
Jerry Bui <bui@mymail.mines.edu>
2019-09-28 19:10:15 -0600

    Made changes esc button - pauses the in game time

--- forgotten_maze.js
+++ forgotten_maze.js
@@ -298,7 +298,7 @@ function menu() {
             forgotten_maze.context.fillText("Down Arrow - Move character down", 125, 250);
             forgotten_maze.context.fillText("Left Arrow - Move character left", 125, 300);
             forgotten_maze.context.fillText("Right Arrow - Move character right", 125, 350);
-            forgotten_maze.context.fillText("Esc - Go to back to main menu", 125, 400);
+            forgotten_maze.context.fillText("Esc - Go to Main Menu (will pause the game)", 125, 400);
 
         } else if (on_developers) {
 
